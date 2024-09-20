3cf4c1a0a0718c3a1bf979e2e477a14d1c26cf24
kenta-kenta <kenken22352235@gmail.com>
2024-09-20 20:07:54 +0900

    検索機能の実装

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -24,6 +24,9 @@
           <x-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
             {{ __('Tweet作成') }}
           </x-nav-link>
+          <x-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
+            {{ __('Tweet検索') }}
+          </x-nav-link>
         </div>
       </div>
 
@@ -84,6 +87,9 @@
       </x-responsive-nav-link>
       <x-responsive-nav-link :href="route('tweets.create')" :active="request()->routeIs('tweets.create')">
         {{ __('Tweet作成') }}
+      </x-responsive-nav-link>
+      <x-responsive-nav-link :href="route('tweets.search')" :active="request()->routeIs('tweets.search')">
+        {{ __('Tweet検索') }}
       </x-responsive-nav-link>
     </div>
 
